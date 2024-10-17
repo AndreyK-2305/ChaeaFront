7e6f5da68e18d92bea5dcc7a138ae1866d667bfe
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-10-17 08:32:12 -0500

    chore: modify App and Docentes log

--- front_chaea/src/components/Docentes_log.js
+++ front_chaea/src/components/Docentes_log.js
@@ -3,11 +3,26 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Iniciar Sesión</h1>
-            {/* Formulario de inicio de sesión aquí */}
-        </div>
-    );
-};
+        <div className="container">
+      {/* Header delgado */}
+      <header className="header">
+        <h1>Título del Componente</h1>
+      </header>
 
+      <div className="main-content">
+        {/* Div izquierdo con la imagen */}
+        <div className="left-div">
+          <img src="ruta_de_tu_imagen.jpg" alt="Descripción de la imagen" className="imagen" />
+        </div>
+
+        {/* Div derecho para el contenido que indiques después */}
+        <div className="right-div">
+          {/* Aquí puedes agregar el contenido que desees */}
+          <h2>Contenido del Div Derecho</h2>
+          <p>Agrega más contenido aquí...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Login;
