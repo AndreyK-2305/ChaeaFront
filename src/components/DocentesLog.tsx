2bdd1fe910f57e86f95f2b1f69ee3b96f5795b1f
JorgeMarles <jamarlesf@gmail.com>
2024-10-17 13:42:38 -0500

    feat: profesor login

---
+++ src/components/DocentesLog.tsx
@@ -0,0 +1,25 @@
+const DocenteLogin = () => {
+    return (
+        <div className="container">
+      {/* Header delgado */}
+      <header className="header">
+        <h1>Título del Componente</h1>
+      </header>
+
+      <div className="main-content">
+        {/* Div izquierdo con la imagen */}
+        <div className="left-div">
+          <img src="ruta_de_tu_imagen.jpg" alt="Descripción de la imagen" className="imagen" />
+        </div>
+
+        {/* Div derecho para el contenido que indiques después */}
+        <div className="right-div">
+          {/* Aquí puedes agregar el contenido que desees */}
+          <h2>Contenido del Div Derecho</h2>
+          <p>Agrega más contenido aquí...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default DocenteLogin;
