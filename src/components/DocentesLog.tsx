18e6fe67383dac0d7663d362a15b7465f6b37aaa
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-10-17 21:38:40 -0500

    feat: visual settings

--- src/components/DocentesLog.tsx
+++ src/components/DocentesLog.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import iconChaea from './iconChaea.jpg';
 import bckDocente from './bckDocente.jpg';
+import googleIcon from './googleIcon.png';
 
 const DocenteLogin = () => {
   return (
@@ -21,7 +22,16 @@ const DocenteLogin = () => {
         <div className="right-div bg-[rgb(249,245,242)] w-1/2 h-screen flex flex-col justify-center items-center space-y-12 pb-24">
           <h1 className='text-[rgb(78,42,149)] text-5xl font-extrabold'>Bienvenid@</h1>
           <h2 className='text-3xl font-extrabold'>Continuar como Docente</h2>
-          <div className='###BOTON GOOGLE###'></div>
+          <div className='###BOTON GOOGLE###'>
+            <button className="flex items-center justify-center bg-white border border-gray-300 rounded-full py-4 px-32 hover:shadow-lg transition duration-200">
+            <img 
+            src={googleIcon} 
+            alt="Google Logo" 
+            className="w-5 h-5 mr-2" 
+            />
+            <span className="text-gray-800 font-semibold text-xl">Iniciar sesión con Google</span>
+            </button>
+          </div>
           <h2 className='text-2xl font-semibold italic'>Para ingresar como estudiante</h2>
           {/*<Link to="###RUTA DEL OTRO COMPONENTE###">*/}
             <h2 className='text-2xl font-semibold italic text-[rgb(10,20,225)]'>pulsa aqui</h2>
