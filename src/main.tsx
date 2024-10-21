5b52b88a365833e046f6b74955f0dd8b70cece0a
AndreyK-2305 <kevinandreyjaimes1@gmail.com>
2024-10-21 09:37:19 -0500

    chore: new branch olny show all views(no functional)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import DocenteLogin from './components/DocentesLog.tsx'
-import EstudianteLogin from './components/EstudiantesLog.tsx'
-import DocentesMain from './components/DocentesMain.tsx'
-import { Routes } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import DocenteLogin from './components/DocentesLog.tsx';
+import EstudianteLogin from './components/EstudiantesLog.tsx';
+import DocentesMain from './components/DocentesMain.tsx';
 
+// Creas el root y usas BrowserRouter para definir las rutas
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <DocentesMain />
+    <Router>
+      <Routes>
+        {/* Define las rutas con sus respectivos componentes */}
+        <Route path="/" element={<App />} />
+        <Route path="/docente-login" element={<DocenteLogin />} />
+        <Route path="/estudiante-login" element={<EstudianteLogin />} />
+        <Route path="/docentes-main" element={<DocentesMain />} />
+      </Routes>
+    </Router>
   </StrictMode>,
-)
+);
